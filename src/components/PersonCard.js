7df318abd6ce1b986f8f4a8e82488e74a145006d
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-06-26 22:37:16 -0600

    Completed assignment

---
+++ src/components/PersonCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    increaseAge = () => {
+        this.setState({ age: parseInt(this.state.age) + 1})
+    }
+
+    render() {
+        const { firstName, lastName, age, hairColor } = this.props;
+        return (
+        <div>
+            <h1>{lastName}, {firstName}</h1>
+            <p>Age: {this.state.age}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick={this.increaseAge}>Add 1 year to {firstName}'s age</button>
+        </div>
+        );
+    }
+}
+
+export default PersonCard;
